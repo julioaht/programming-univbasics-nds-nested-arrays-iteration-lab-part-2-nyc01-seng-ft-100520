46d3fd800841916da6d3b71a9264b024ddb8eaef
julio hernandez <julioandres72@gmail.com>
2020-09-28 17:28:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ min_temps_arr = []
 row_index = 0
 while row_index < src.count do
   element_index = 0
-  smallest_num = 0
+  smallest_num = 100
   while element_index < src[row_index].count do
     if src[row_index][element_index] < smallest_num
       smallest_num = src[row_index][element_index]
