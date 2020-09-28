212a591e9d8b4fef72908f4d826a9e1073a77580
julio hernandez <julioandres72@gmail.com>
2020-09-28 17:26:31 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ row_index = 0
 smallest_num = 100
 while row_index < src.count do
   element_index = 0
-  smallest_num = 0
+  smallest_num = 100
   while element_index < src[row_index].count do
     if src[row_index][element_index] <  smallest_num
     end
